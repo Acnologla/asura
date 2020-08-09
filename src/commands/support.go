1d82ba665186b5c86427e7612066641057b47e35
rafaeeels <rafaeelsgamer@gmail.com>
2020-08-09 19:01:08 -0300

    Added 'support' command

---
+++ src/commands/support.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"asura/src/handler"
+	"context"
+	"github.com/andersfylling/disgord"
+)
+
+func init() {
+	handler.Register(handler.Command{
+		Aliases:   []string{"suporte","servidor"},
+		Run:       runSupport,
+		Available: true,
+		Cooldown:  1,
+		Usage:     "j!suporte",
+		Help:      "Veja meu servidor de suporte",
+	})
+}
+
+func runSupport(session disgord.Session, msg *disgord.Message, args []string) {
+	msg.Reply(context.Background(), session, "https://discord.gg/tdVWQGV")
+}
