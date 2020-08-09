ef9017a059dff5485b63fafda707888f0d2ae291
rafaeeels <rafaeelsgamer@gmail.com>
2020-08-09 19:00:39 -0300

    Added 'escrever' command

---
+++ src/commands/escrever.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"asura/src/handler"
+	"context"
+	"strings"
+	"github.com/andersfylling/disgord"
+)
+
+func init() {
+	handler.Register(handler.Command{
+		Aliases:   []string{"escrever","escrita","mock"},
+		Run:       runEscrever,
+		Available: true,
+		Cooldown:  1,
+		Usage:     "j!escrever <texto>",
+		Help:      "Escreva igual autista",
+	})
+}
+
+func runEscrever(session disgord.Session, msg *disgord.Message, args []string) {
+	text := ""
+	str:= strings.Join(args," ")
+	for i:=0; i < len(str);i++{
+		if i % 2 ==0{
+			text += strings.ToUpper(string(str[i]))
+		}else{
+			text += strings.ToLower(string(str[i]))
+		}
+	}
+	msg.Reply(context.Background(), session, msg.Author.Mention() + ", " + text)
+}
