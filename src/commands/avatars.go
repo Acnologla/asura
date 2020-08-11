7927fe76c03126b2960c1a3bb6a3cedd5eb4e4ee
rafaeeels <rafaeelsgamer@gmail.com>
2020-08-10 21:54:01 -0300

    Added 'avatars' command

---
+++ src/commands/avatars.go
@@ -0,0 +1,83 @@
+package commands
+
+import (
+	"asura/src/handler"
+	"asura/src/utils"
+	"context"
+	"strconv"
+	"asura/src/database"
+	"fmt"
+	"github.com/andersfylling/disgord"
+)
+
+func init() {
+	handler.Register(handler.Command{
+		Aliases:   []string{"oldavatars", "avatars"},
+		Run:       runAvatars,
+		Available: true,
+		Cooldown:  5,
+		Usage:     "j!oldavatars <usuario>",
+		Help:      "Veja os avatares antigos de alguem",
+	})
+}
+
+func runAvatars(session disgord.Session, msg *disgord.Message, args []string) {
+	user := utils.GetUser(msg, args)
+	ctx:= context.Background()
+	var userinfo database.User
+	var private bool
+	id := strconv.FormatUint(uint64(user.ID),10)
+	database.Database.NewRef("users/"+id).Get(ctx, &userinfo)
+	database.Database.NewRef("private/"+id).Get(ctx, &private)
+	if private{
+		msg.Reply(ctx,session,msg.Author.Mention() + ", O historico de avatar desse usuario é privado")
+		return
+	}
+	if len(userinfo.Avatars) ==  0 {
+		msg.Reply(ctx,session,msg.Author.Mention() + ", Não tenho o historico de avatares desse usuario")
+		return
+	}
+	count:=0
+	message,_ := msg.Reply(context.Background(), session, &disgord.CreateMessageParams{
+		Embed: &disgord.Embed{
+			Color:       65535,
+			Title: fmt.Sprintf("Avatar numero %d",count+1),
+			Image: &disgord.EmbedImage{
+				URL: userinfo.Avatars[0],
+			},
+		},
+	})
+	message.React(ctx,session,"⬅️")
+	message.React(ctx,session,"➡️")
+	handler.RegisterHandler(message, func(removed bool, emoji disgord.Emoji,u disgord.Snowflake) {
+		if !removed && msg.Author.ID == u{
+			msgUpdater := handler.Client.UpdateMessage(ctx,msg.ChannelID,message.ID)
+			if emoji.Name == "⬅️"{
+				if count == 0{
+					count = len(userinfo.Avatars) -1 
+				}else{
+					count--
+				}
+			}
+			if emoji.Name == "➡️"{
+				if count == len(userinfo.Avatars) -1{
+					count = 0 
+				}else{
+					count++
+				}
+			}
+			if emoji.Name == "➡️" || emoji.Name =="⬅️"{
+				msgUpdater.SetEmbed(&disgord.Embed{
+					Color:       65535,
+					Title: fmt.Sprintf("Avatar numero %d",count+1),
+					Image: &disgord.EmbedImage{
+						URL: userinfo.Avatars[count],
+					},
+				})
+				msgUpdater.Execute()
+				handler.Client.DeleteUserReaction(ctx,msg.ChannelID,message.ID,u,emoji.Name)
+			}
+		}
+	},60*10)
+
+}
