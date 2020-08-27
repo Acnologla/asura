7fd12a7fde0901a25f38b9277d7030b31e8a5319
rafaeeels <rafaeelsgamer@gmail.com>
2020-08-27 15:22:05 -0300

    Added upgades command

---
+++ src/commands/upgrades.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"asura/src/handler"
+	"context"
+	"github.com/andersfylling/disgord"
+	"fmt"
+)
+
+func init() {
+	handler.Register(handler.Command{
+		Aliases:   []string{"galoupgrades", "upgrades"},
+		Run:       runUpgrades,
+		Available: true,
+		Cooldown:  3,
+		Usage:     "j!upgrades",
+		Help:      "Veja os upgrades apra o seu galo",
+	})
+}
+
+func runUpgrades(session disgord.Session, msg *disgord.Message, args []string) {
+	var text string
+	for key,value := range upgrades{
+		text += fmt.Sprintf("[Level %d] - %s\n",value,key)
+	}
+	msg.Reply(context.Background(), session, &disgord.CreateMessageParams{
+		Embed: &disgord.Embed{
+			Title: "Upgrades",
+			Color: 65535,
+			Description: text,
+		},
+	})
+}
