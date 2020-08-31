4c09a3ee6e9ce5181b550de646e756d37b286c45
rafaeeels <rafaeelsgamer@gmail.com>
2020-08-31 20:53:08 -0300

    Added eval command

---
+++ src/commands/eval.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"asura/src/handler"
+	"context"
+	"fmt"
+	"asura/src/utils"
+	"strings"
+	"github.com/andersfylling/disgord"
+	"asura/src/interpreter"
+)
+
+func init() {
+	handler.Register(handler.Command{
+		Aliases:   []string{"eval", "evl"},
+		Run:       runEval,
+		Available: false,
+		Cooldown:  1,
+		Usage:     "j!eval",
+		Help:      "eval",
+	})
+}
+
+func runEval(session disgord.Session, msg *disgord.Message, args []string) {
+	if msg.Author.ID != utils.StringToID("365948625676795904"){
+		return
+	}
+	if len(args) > 0 {
+		code := strings.Join(args," ")
+		if strings.HasPrefix(code,"```") && strings.HasSuffix(code,"```"){
+			code = code[3:]
+			code = code[:len(code)-3]
+			if strings.HasPrefix(code,"rust"){
+				code = code[4:]
+			}
+		}	
+		eval := interpreter.Run(code)
+		msg.Reply(context.Background(),session,fmt.Sprintf("```js\n%v ```",eval))
+	}
+}
