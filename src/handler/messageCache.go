4b0d7510ce1b14c3990175d6089c837418492e41
unknown <rafaeelsgamer@gmail.com>
2021-01-03 01:42:02 -0300

    Fixed race conditions

--- src/handler/messageCache.go
+++ src/handler/messageCache.go
@@ -1,71 +1,81 @@
 package handler
-
+ 
 import (
-	"github.com/andersfylling/disgord"
+    "github.com/andersfylling/disgord"
 	"sync"
 	"time"
 )
-
+ 
 type Message struct{
-	disgord.Message
-	sync.RWMutex
-	Date time.Time
+    disgord.Message
+    sync.RWMutex
+    Date time.Time
 }
-
+ 
 type Cache struct{
-	*disgord.CacheLFUImmutable
-	Messages map[disgord.Snowflake]*Message
+    sync.RWMutex
+    *disgord.CacheLFUImmutable
+    Messages map[disgord.Snowflake]*Message
 }
-
+ 
 func (cache *Cache) MessageCreate(data []byte) (*disgord.MessageCreate, error){
-	msg, err := cache.CacheNop.MessageCreate(data)
-	message := msg.Message
-	if err != nil {
-		return msg, err
-	}
-	if message.Author != nil && message.Type == disgord.MessageTypeDefault {
-		if !message.Author.Bot{
-			cache.Messages[message.ID] = &Message{
-				Message: *message,
-				Date: time.Now(),
-			}
-		}
-	}
-	return msg, nil
+    msg, err := cache.CacheNop.MessageCreate(data)
+    if err != nil {
+        return nil, err
+    }
+    message := msg.Message
+    if message.Author != nil && message.Type == disgord.MessageTypeDefault {
+        if !message.Author.Bot{
+            entry := &Message{
+                Message: *(disgord.DeepCopy(message).(*disgord.Message)),
+                Date: time.Now(),
+            }
+ 
+            cache.Lock()
+            cache.Messages[message.ID] = entry
+            cache.Unlock()
+        }
+    }
+    return msg, nil
 }
-
+ 
 func (cache *Cache) MessageUpdate(data []byte) (*disgord.MessageUpdate, error){
-	msg, err := cache.CacheNop.MessageUpdate(data)
-	if err != nil {
-		return msg, err
-	}
-	oldMsg, ok := cache.Messages[msg.Message.ID]
-	if ok{
-		oldMsg.Lock()
-		oldMsgCopy := oldMsg.Message
-		go OnMessageUpdate(&oldMsgCopy, msg.Message)
-		oldMsg.Message = *msg.Message
-		oldMsg.Date = time.Now()
-		oldMsg.Unlock()
-	}
-	return msg, nil
+    msg, err := cache.CacheNop.MessageUpdate(data)
+    if err != nil {
+        return msg, err
+    }
+    cache.RLock()
+    oldMsg, ok := cache.Messages[msg.Message.ID]
+    cache.RUnlock()
+    if ok{ 
+        oldMsg.Lock()
+        oldMsgCopy := oldMsg.Message
+        go OnMessageUpdate(&oldMsgCopy, msg.Message)
+        oldMsg.Message = *msg.Message
+        oldMsg.Date = time.Now()
+        oldMsg.Unlock()
+    }
+    return msg, nil
 }
-
-
+ 
+ 
 func init(){
-	//clear 30 minutes old messages
-	go func(){
+    //clear N minutes old messages
+    const N = 30
+    go func(){
 		time.Sleep(time.Minute * 30)
-		for{
-			cache := Client.Cache().(*Cache)
-			for _, msg := range cache.Messages{
-				msg.Lock()
-				if time.Since(msg.Date).Minutes() >= 30{
-					delete(cache.Messages, msg.ID)
-				}
-				msg.Unlock()
-			}
-			time.Sleep(time.Minute)
-		}
-	}()
+        ticker := time.NewTicker(time.Duration(N) * time.Minute)
+        cache := Client.Cache().(*Cache)
+        for {
+			now := time.Now()
+            cache.Lock()
+            for id, msg := range cache.Messages{
+                if now.Sub(msg.Date).Minutes() >= N {
+                    delete(cache.Messages, id)
+                }
+            }
+			cache.Unlock()
+			<-ticker.C
+        }
+    }()
 }
