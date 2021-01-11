100618b7625e22b39e3fa42f8aa390087cce3053
chiyoku <chiyokuoni@gmail.com>
2021-01-11 09:50:33 -0300

    Added profile image and dungeon resets that makes the NPC 3x more difficult it time it resets

--- src/commands/profile.go
+++ src/commands/profile.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func init() {
@@ -21,7 +22,7 @@ func init() {
 		Aliases:   []string{"perfil", "profile"},
 		Run:       runProfile,
 		Available: true,
-		Cooldown:  5,
+		Cooldown:  15,
 		Usage:     "j!profile @user",
 		Help:      "Veja o perfil",
 		Category: 1,
@@ -49,19 +50,8 @@ func GetClanPos(clanName string) int{
 }
 
 func runProfile(session disgord.Session, msg *disgord.Message, args []string) {
-	// Loads the template of profile.png in memory
-	file, err := os.Open("resources/galo/profile.png")
-	defer file.Close()
-
-	// Decodes it into a image.Image
-	img, err := png.Decode(file)
-	if err != nil {
-		return
-	}
-
 	// Download user image
 	user := utils.GetUser(msg,args,session)
-	galo, _ := rinha.GetGaloDB(user.ID)
 	url, _ := user.AvatarURL(128, false)
 	replacer := strings.NewReplacer(".gif", ".png", ".webp", ".png")
 	avatar, err := utils.DownloadImage(replacer.Replace(url))
@@ -70,23 +60,109 @@ func runProfile(session disgord.Session, msg *disgord.Message, args []string) {
 		msg.Reply(context.Background(), session, "Invalid image")
 		return
 	}
-	// Resize the images
-	avatar = resize.Resize(99, 99, avatar, resize.Lanczos3)
-	name := "Galo do " + user.Username
-	if galo.Name != "" {
-		name = galo.Name
-	}
-	println(name)
-	// Here we draw the image to the "editor"
-	dc := gg.NewContext(419, 306)
-	dc.LoadFontFace("resources/Raleway-Black.ttf", 1)
-	dc.DrawImage(img, 0, 0)
 
-	dc.DrawCircle(72, 142, 46)
+	radius := 70.0
+	// Resize the images
+	avatar = resize.Resize(uint(radius*2), uint(radius*2), avatar, resize.Lanczos3)
+	
+	file, err := os.Open("resources/wall.png")
+	
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+	img, err := png.Decode(file)
+
+	if err != nil {
+		return
+	}
+
+	dc := gg.NewContext(600, 430)
+
+	dc.DrawRoundedRectangle(0, 0, 600, 430, 10)
+	dc.Fill()
+
+	dc.DrawRoundedRectangle(0, 0, 600, 200, 10)
 	dc.Clip()
-	// clipar avatar antes
-	dc.DrawImage(avatar, 20, 89)
+	dc.DrawImage(img,0,0)
 	dc.ResetClip()
+
+	dc.DrawRoundedRectangle(0, 0, 600, 430, 10)
+	dc.Clip()
+
+	dc.SetRGB(0.8,0.31,0.31)
+	dc.DrawRectangle(0, 170, 600, 50)
+	dc.Fill()
+
+	dc.SetRGB(0.9,0.9,0.9)
+	dc.DrawRectangle(0, 220, 200, 220)
+	dc.Fill()
+
+	dc.DrawCircle(100, 190, radius+8)
+	dc.SetRGB(1, 1, 1)
+	dc.Fill()
+	dc.SetRGB(0, 0, 0)
+	dc.DrawCircle(100, 190, radius)
+	dc.Clip()
+	dc.Fill()
+
+	dc.DrawImage(avatar, int(100-radius), int(190-radius))
+
+	dc.ResetClip()
+	dc.SetRGB(0.3, 0.3, 0.3)
+
+	err = dc.LoadFontFace("./resources/Raleway-Bold.ttf", 13)
+	dc.DrawString("GALOS", 220, 245)
+	dc.DrawLine(220,250,580,250)
+	dc.Stroke()
+
+	dc.DrawString("INFO", 220, 345)
+	dc.DrawLine(220,350,580,350)
+	dc.Stroke()
+
+	galo, _ := rinha.GetGaloDB(user.ID)
+
+	err = dc.LoadFontFace("./resources/Raleway-Bold.ttf", 16)
+	dc.DrawStringAnchored("WINS", 260, 380, 0.5, 0.5)
+	dc.DrawStringAnchored("LOSES", 350, 380, 0.5, 0.5)
+	dc.DrawStringAnchored("CLAN", 440, 380, 0.5, 0.5)
+	dc.DrawStringAnchored("GALOS", 530, 380, 0.5, 0.5)
+
+	err = dc.LoadFontFace("./resources/Raleway-Bold.ttf", 25)
+	dc.DrawStringAnchored(strconv.Itoa(galo.Win), 260, 400, 0.5, 0.5)
+	dc.DrawStringAnchored(strconv.Itoa(galo.Lose), 350, 400, 0.5, 0.5)
+	dc.DrawStringAnchored("#" + strconv.Itoa(GetClanPos(galo.Clan)), 440, 400, 0.5, 0.5)
+	dc.DrawStringAnchored(strconv.Itoa(len(galo.Galos)+1), 530, 400, 0.5, 0.5)
+
+	err = dc.LoadFontFace("./resources/Raleway-Light.ttf", 13)
+
+	dc.DrawString("Clan", 10, 280)
+	dc.DrawString("Money", 10, 310)
+	dc.DrawStringAnchored(galo.Clan, 190, 280, 1, 0)
+	dc.DrawStringAnchored(strconv.Itoa(galo.Money), 190, 310, 1, 0)
+
+	dc.SetRGB(1, 1, 1)
+	err = dc.LoadFontFace("./resources/Raleway-Light.ttf", 25)
+	dc.DrawStringAnchored(msg.Author.Username, 300, 195, 1, 0.5)
+
+	dc.SetRGB(0.3,0.3,0.3)
+	dc.DrawLine(10,320,190,320)
+	dc.Stroke()
+
+	dc.SetRGB(0.7, 0.7, 0.7)
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 2; j++ {
+			dc.DrawRectangle(float64(10 + i*47), float64(332 + j*47), 40, 40)
+			dc.Fill()
+		}
+	}
+
+	dc.SetRGB(0.9, 0.9, 0.9)
+	for i := 0; i < 5; i++ {
+		dc.DrawRectangle(float64(220 + i*75), 265, 55, 55)
+		dc.Fill()
+	}
 
 	// And here we encode it to send
 	var b bytes.Buffer
