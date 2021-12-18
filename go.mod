39084e53123452181d1129270588ee64deae32f8
chiyoku <chiyokuoni@gmail.com>
2021-12-18 19:26:24 -0300

    refactor: removed everything

--- go.mod
+++ go.mod
@@ -3,23 +3,14 @@ module asura
 go 1.16
 
 require (
-	cloud.google.com/go/firestore v1.3.0 // indirect
-	firebase.google.com/go v3.13.0+incompatible
-	github.com/acnologla/interpreter v0.0.0-20201213223129-6a55692d2db7
-	github.com/agnivade/levenshtein v1.1.0
-	github.com/andersfylling/disgord v0.26.11-0.20210607174434-0861da98427e
-	github.com/cjsaylor/chessimage v0.0.0-20190107020940-8abad33612f4
-	github.com/flopp/go-findfont v0.0.0-20200805110358-089b91d05de8 // indirect
-	github.com/fogleman/gg v1.3.1-0.20210928143535-8febc0f526ad
-	github.com/joho/godotenv v1.3.0
-	github.com/klauspost/compress v1.12.3 // indirect
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
-	github.com/notnil/chess v1.2.0
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	github.com/andersfylling/disgord v0.29.0 // indirect
+	github.com/cjsaylor/chessimage v0.0.0-20190107020940-8abad33612f4 // indirect
+	github.com/flopp/go-findfont v0.1.0 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/uptrace/bun v1.0.19 // indirect
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
-	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
-	gonum.org/v1/plot v0.8.1
-	google.golang.org/api v0.34.0
-	nhooyr.io/websocket v1.8.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,58 +1,7 @@
 package main
 
-import (
-	_ "asura/src/commands" // Initialize all commands and put them into an array
-	"asura/src/database"
-	"asura/src/handler"
-	"asura/src/telemetry"
-	"fmt"
-	"math/rand"
-	"os"
-	"time"
+import "fmt"
 
-	"github.com/andersfylling/disgord"
-	"github.com/joho/godotenv"
-)
-
-func onReady() {
-	myself, err := handler.Client.Cache().GetCurrentUser()
-	handler.Client.UpdateStatusString("Use j!comandos para ver meus comandos")
-	if err == nil {
-		telemetry.Info(fmt.Sprintf("%s Started", myself.Username), map[string]string{
-			"eventType": "ready",
-		})
-	}
-	go telemetry.MetricUpdate(handler.Client)
-
-}
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	//If it's not in production so it's good to read a ".env" file
-	if os.Getenv("PRODUCTION") == "" {
-		err := godotenv.Load()
-		if err != nil {
-			panic("Cannot read the motherfucking envfile")
-		}
-	}
-	//Initialize datalog services for telemetry of the application
-	telemetry.Init()
-	database.Init()
-	fmt.Println("Starting bot...")
-	cache := disgord.NewBasicCache()
-	client := disgord.New(disgord.Config{
-		RejectEvents: []string{disgord.EvtPresenceUpdate, disgord.EvtTypingStart},
-		BotToken:     os.Getenv("TOKEN"),
-		Cache: &handler.Cache{
-			BasicCache: cache,
-			Messages:   map[disgord.Snowflake]*handler.Message{},
-		},
-	})
-	defer client.Gateway().StayConnectedUntilInterrupted()
-	handler.Client = client
-	client.Gateway().MessageCreate(handler.OnMessage)
-	client.Gateway().MessageReactionAdd(handler.OnReactionAdd)
-	client.Gateway().MessageReactionRemove(handler.OnReactionRemove)
-	client.Gateway().InteractionCreate(handler.Interaction)
-	client.Gateway().BotReady(onReady)
-
+	fmt.Printf("Tutturu ! ! !")
 }
