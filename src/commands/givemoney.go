a5a48c774c62c61080f05c98ea14535a45fd3875
leo <80543539+justleoo@users.noreply.github.com>
2022-01-27 18:15:11 -0300

    fixing portuguese errors

--- src/commands/givemoney.go
+++ src/commands/givemoney.go
@@ -17,7 +17,7 @@ func init() {
 		Available: true,
 		Cooldown:  5,
 		Usage:     "j!givemoney <user>",
-		Help:      "De dinheiro para alguem",
+		Help:      "Dê dinheiro para alguem",
 		Category:  1,
 	})
 }
@@ -29,15 +29,15 @@ func runGiveMoney(session disgord.Session, msg *disgord.Message, args []string) 
 	}
 	value, err := strconv.Atoi(args[1])
 	if err != nil {
-		msg.Reply(context.Background(), session, msg.Author.Mention()+", Quantia invalida")
+		msg.Reply(context.Background(), session, msg.Author.Mention()+", Quantia inválida")
 		return
 	}
 	if 0 >= value {
-		msg.Reply(context.Background(), session, msg.Author.Mention()+", Quantia invalida")
+		msg.Reply(context.Background(), session, msg.Author.Mention()+", Quantia inválida")
 		return
 	}
 	if msg.Mentions[0].ID == msg.Author.ID || msg.Mentions[0].Bot {
-		msg.Reply(context.Background(), session, msg.Author.Mention()+", Usuario invalido")
+		msg.Reply(context.Background(), session, msg.Author.Mention()+", Usuário inválido")
 		return
 	}
 	err = rinha.ChangeMoney(msg.Author.ID, -value, value)
@@ -51,9 +51,9 @@ func runGiveMoney(session disgord.Session, msg *disgord.Message, args []string) 
 			"reciever": strconv.FormatUint(uint64(user.ID), 10),
 		})
 		rinha.ChangeMoney(msg.Mentions[0].ID, value, 0)
-		msg.Reply(context.Background(), session, fmt.Sprintf("%s, Voce deu **%d** de dinheiro a %s com sucesso", msg.Author.Mention(), value, msg.Mentions[0].Username))
+		msg.Reply(context.Background(), session, fmt.Sprintf("%s, Você deu **%d** de dinheiro a %s com sucesso", msg.Author.Mention(), value, msg.Mentions[0].Username))
 	} else {
-		msg.Reply(context.Background(), session, msg.Author.Mention()+", Voce nao tem essa quantia use j!lootbox para ver seu dinheiro")
+		msg.Reply(context.Background(), session, msg.Author.Mention()+", Vocnao tem essa quantia use j!lootbox para ver seu dinheiro")
 		return
 	}
 }
