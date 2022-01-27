bc28076c850ccce43527f5dcc93d329c7e6b4bac
leo <80543539+justleoo@users.noreply.github.com>
2022-01-27 18:03:54 -0300

    fixing portuguese errors

--- src/commands/daily.go
+++ src/commands/daily.go
@@ -18,14 +18,14 @@ func init() {
 		Cooldown:  5,
 		Usage:     "j!daily",
 		Category:  1,
-		Help:      "Receba seu bonus diario",
+		Help:      "Receba seu bônus diário",
 	})
 }
 
 func runDaily(session disgord.Session, msg *disgord.Message, args []string) {
 	galo, _ := rinha.GetGaloDB(msg.Author.ID)
 	if galo.Type == 0 {
-		msg.Reply(context.Background(), session, msg.Author.Mention()+", Voce nao tem um galo, use j!galo para criar um")
+		msg.Reply(context.Background(), session, msg.Author.Mention()+", Você não tem um galo, use j!galo para criar um")
 		return
 	}
 	topGGCalc := (uint64(time.Now().Unix()) - galo.Daily.Voted) / 60 / 60 / 12
@@ -57,12 +57,12 @@ func runDaily(session disgord.Session, msg *disgord.Message, args []string) {
 		msg.Reply(context.Background(), session, &disgord.Embed{
 			Color:       65535,
 			Title:       "Daily",
-			Description: fmt.Sprintf("Voce ganhou **%d** de dinheiro, **%d** de xp\n\nStrike: **%d**", money, xp, strike),
+			Description: fmt.Sprintf("Você ganhou **%d** de dinheiro, **%d** de xp\n\nStrike: **%d**", money, xp, strike),
 		})
 	} else {
 		need := uint64(time.Now().Unix()) - galo.Daily.Voted
 		if topGGCalc >= 1 && !voted {
-			msg.Reply(context.Background(), session, fmt.Sprintf("%s, para pegar o bonus diario voce precisa votar em mim.\n Voce pode pegar o bonus diario a cada 12 horas\nLink para votar:\nhttps://top.gg/bot/470684281102925844", msg.Author.Mention()))
+			msg.Reply(context.Background(), session, fmt.Sprintf("%s, para pegar o bônus diario voce precisa votar em mim.\n Você pode pegar o bônus diario a cada 12 horas\nLink para votar:\nhttps://top.gg/bot/470684281102925844", msg.Author.Mention()))
 		} else {
 			msg.Reply(context.Background(), session, fmt.Sprintf("%s, faltam **%d** horas e **%d** minutos para voce poder usar o daily novamente", msg.Author.Mention(), 11-(need/60/60), 59-(need/60%60)))
 		}
