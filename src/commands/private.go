b199a1e289149fe1bc9953c1aa55aec02efca001
acno logia <rafaeelsgamer@gmail.com>
2022-01-30 11:51:28 -0300

    Added private command

---
+++ src/commands/private.go
@@ -0,0 +1,49 @@
+package commands
+
+import (
+	"asura/src/firebase"
+	"asura/src/handler"
+	"context"
+	"strconv"
+
+	"asura/src/translation"
+
+	"github.com/andersfylling/disgord"
+)
+
+func init() {
+	handler.RegisterCommand(handler.Command{
+		Name:        "private",
+		Description: translation.T("PrivateHelp", "pt"),
+		Run:         runPrivate,
+		Cooldown:    5,
+	})
+}
+
+func runPrivate(itc *disgord.InteractionCreate) *disgord.InteractionResponse {
+	var acc bool
+	id := strconv.FormatUint(uint64(itc.Member.UserID), 10)
+	ctx := context.Background()
+	if err := firebase.Database.NewRef("private/"+id).Get(ctx, &acc); err != nil {
+		return nil
+	}
+	newVal := !acc
+	if err := firebase.Database.NewRef("private/"+id).Set(ctx, &(newVal)); err != nil {
+		return nil
+	}
+	if acc {
+		return &disgord.InteractionResponse{
+			Type: disgord.InteractionCallbackChannelMessageWithSource,
+			Data: &disgord.InteractionApplicationCommandCallbackData{
+				Content: translation.T("PrivateDisabled", translation.GetLocale(itc)),
+			},
+		}
+	} else {
+		return &disgord.InteractionResponse{
+			Type: disgord.InteractionCallbackChannelMessageWithSource,
+			Data: &disgord.InteractionApplicationCommandCallbackData{
+				Content: translation.T("PrivateEnabled", translation.GetLocale(itc)),
+			},
+		}
+	}
+}
