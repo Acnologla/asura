4498435b9586992748b8832d9e26dfaf309d0e19
acno logia <rafaeelsgamer@gmail.com>
2022-02-20 16:58:01 -0300

    Added upgrades command

---
+++ src/commands/upgrades.go
@@ -0,0 +1,94 @@
+package commands
+
+import (
+	"asura/src/database"
+	"asura/src/entities"
+	"asura/src/handler"
+	"asura/src/rinha"
+	"asura/src/utils"
+	"fmt"
+	"strings"
+
+	"asura/src/translation"
+
+	"github.com/andersfylling/disgord"
+)
+
+func init() {
+	handler.RegisterCommand(handler.Command{
+		Name:        "upgrades",
+		Description: translation.T("UpgradesHelp", "pt"),
+		Run:         runUpgrades,
+		Cooldown:    12,
+		Options: utils.GenerateOptions(&disgord.ApplicationCommandOption{
+			Type:        disgord.OptionTypeNumber,
+			Name:        "upgrade_id",
+			Description: translation.T("UpgradesId", "pt"),
+			MinValue:    0,
+			MaxValue:    4,
+		}),
+		Category: handler.Profile,
+	})
+}
+
+func runUpgrades(itc *disgord.InteractionCreate) *disgord.InteractionResponse {
+	user := itc.Member.User
+	galo := database.User.GetUser(user.ID)
+	if len(itc.Data.Options) == 0 {
+		desc := fmt.Sprintf("Upgrade Xp: **%d/%d**", galo.Xp, rinha.CalcUserXp(&galo))
+		if len(galo.Upgrades) > 0 {
+			desc += "\n\nUpgrades:\n"
+			upgrades := rinha.Upgrade{
+				Childs: rinha.Upgrades,
+			}
+			for i, upgrade := range galo.Upgrades {
+				upgrades = upgrades.Childs[upgrade]
+				v := strings.Repeat("-", i*5)
+				desc += fmt.Sprintf("%s%s\n%s%s\n", v, upgrades.Name, v, upgrades.Value)
+			}
+		}
+		if rinha.HavePoint(&galo) {
+			desc += translation.T("UpgradeDesc", translation.GetLocale(itc), rinha.UpgradesToString(&galo))
+		}
+		return &disgord.InteractionResponse{
+			Type: disgord.InteractionCallbackChannelMessageWithSource,
+			Data: &disgord.InteractionApplicationCommandCallbackData{
+				Embeds: []*disgord.Embed{{
+					Title:       "Upgrades",
+					Color:       65535,
+					Description: desc,
+				}},
+			},
+		}
+	}
+	i := int(itc.Data.Options[0].Value.(float64))
+
+	if !rinha.HavePoint(&galo) {
+		return &disgord.InteractionResponse{
+			Type: disgord.InteractionCallbackChannelMessageWithSource,
+			Data: &disgord.InteractionApplicationCommandCallbackData{
+				Content: translation.T("NoUpgrade", translation.GetLocale(itc)),
+			},
+		}
+	}
+	upgrades := rinha.GetCurrentUpgrade(&galo)
+	if 0 > i || i >= len(upgrades.Childs) {
+		return &disgord.InteractionResponse{
+			Type: disgord.InteractionCallbackChannelMessageWithSource,
+			Data: &disgord.InteractionApplicationCommandCallbackData{
+				Content: translation.T("InvalidUpgradeID", translation.GetLocale(itc)),
+			},
+		}
+	}
+	database.User.UpdateUser(user.ID, func(u entities.User) entities.User {
+		u.Upgrades = append(u.Upgrades, i)
+		return u
+	})
+	return &disgord.InteractionResponse{
+		Type: disgord.InteractionCallbackChannelMessageWithSource,
+		Data: &disgord.InteractionApplicationCommandCallbackData{
+			Content: translation.T("NewUpgrade", translation.GetLocale(itc), upgrades.Childs[i].Name),
+		},
+	}
+
+}
